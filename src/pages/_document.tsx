c29c3046966184dac7a0a7c749bc447ffab312a4
DanBus0 <danbusichio@gmail.com>
2025-07-09 15:34:23 -0400

    Add files via upload

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,22 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Analytics 4 */}
+        <script 
+          async 
+          src="https://www.googletagmanager.com/gtag/js?id=G-QCKWZ9DN08"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-QCKWZ9DN08');
+            `,
+          }}
+        />
+        
         {/* 
           CRITICAL: DO NOT REMOVE THIS SCRIPT
           The Softgen AI monitoring script is essential for core app functionality.
