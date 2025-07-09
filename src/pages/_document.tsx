66fd8e9227197dcba6c885799398d1363c483155
DanBus0 <danbusichio@gmail.com>
2025-07-09 16:46:44 -0400

    Add files via upload

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* Favicon */}
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon-ico-mcwff38j.png" type="image/png" />
         
         {/* Google Analytics 4 */}
         <script 
@@ -32,7 +32,7 @@ export default function Document() {
               "@type": "Organization",
               "name": "OverLeased",
               "url": "https://www.overleased.com",
-              "logo": "https://www.overleased.com/favicon.ico"
+              "logo": "https://www.overleased.com/favicon-ico-mcwff38j.png"
             })
           }}
         />
